1ecc59cbc51868e4562d756b0417b842719d614d
dixit5 <gandhi.dixit5@gmail.com>
2019-06-04 09:01:28 -0400

    Initial commit

---
+++ app/src/main/java/com/example/exam/MainActivity.java
@@ -0,0 +1,115 @@
+package com.example.exam;
+
+import android.os.PersistableBundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etName, etLast, etEmail, etPass;
+    Button btnSubmit, btnExit;
+    ImageView ivImage;
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Life Cycle", "OnStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Life Cycle", "OnPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Life Cycle", "OnDestroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Life Cycle", "OnStop");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+            etName = findViewById(R.id.etName);
+            etLast = findViewById(R.id.etLast);
+            etPass = findViewById(R.id.etPass);
+            etEmail = findViewById(R.id.etEmail);
+            ivImage = findViewById(R.id.ivImage);
+            btnSubmit = findViewById(R.id.btnSubmit);
+            btnExit = findViewById(R.id.btnExit);
+
+
+
+
+
+            btnSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (etName.getText().toString().isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Please enter Name", Toast.LENGTH_LONG).show();
+                    } else if (etLast.getText().toString().isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Please enter Last Name", Toast.LENGTH_LONG).show();
+
+                    } else if (etPass.getText().toString().isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Please enter Password", Toast.LENGTH_LONG).show();
+
+                    } else if (etEmail.getText().toString().isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Please enter Email", Toast.LENGTH_LONG).show();
+
+                    } else {
+
+
+                        Snackbar snack = Snackbar.make(findViewById(R.id.conLayout), "Thank You", Snackbar.LENGTH_LONG).setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                Toast.makeText(MainActivity.this, "Reset", Toast.LENGTH_LONG).show();
+                                etName.setText(" ");
+                                etLast.setText(" ");
+                                etPass.setText(" ");
+                                etEmail.setText(" ");
+                            }
+                        });
+                        snack.show();
+                    }
+                }
+            });
+
+            Button btnlogout = (Button) findViewById(R.id.btnExit);
+
+            btnlogout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+
+                    finish();
+                    moveTaskToBack(true);
+                }
+            });
+
+
+        }
+
+
+    }
+
